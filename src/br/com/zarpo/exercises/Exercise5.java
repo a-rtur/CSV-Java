539678a91ecebb03c25e25d1936937a4a946ff1b
a-rtur <arturfulgonixavier@gmail.com>
2018-04-06 10:40:57 -0300

    Exercise 5

--- src/br/com/zarpo/exercises/Exercise5.java
+++ src/br/com/zarpo/exercises/Exercise5.java
@@ -1,7 +1,55 @@
 package br.com.zarpo.exercises;
 
+import java.io.*;
+import java.util.*;
+
+/**
+ *
+ * @author Artur
+ */
 public class Exercise5 {
-    public static void main(String[] args) {
-        
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String csvMembers = "src\\br\\com\\zarpo\\data\\Members.csv";
+        String csvVisits = "src\\br\\com\\zarpo\\data\\Visits.csv";
+        BufferedReader brM = null;
+        BufferedReader brV = null;
+        String lineM = "";
+        String lineV = "";
+        int incomeA = 0;
+        int incomeB = 0;
+        int incomeC = 0;
+        ArrayList<Integer> dontCheck = new ArrayList<Integer>();
+        try {
+            brV = new BufferedReader(new FileReader(csvVisits));
+            brV.readLine();
+            while ((lineV = brV.readLine()) != null) {
+                String[] columnsV = lineV.split(",");
+                if (Integer.parseInt(columnsV[3]) > 0) {
+                    brM = new BufferedReader(new FileReader(csvMembers));
+                    brM.readLine();
+                    while ((lineM = brM.readLine()) != null) {
+                        String columnsM[] = lineM.split(",");
+                        if (Integer.parseInt(columnsV[1]) == Integer.parseInt(columnsM[0])) {
+                            if (columnsM[2].equals("A")) {
+                                incomeA = incomeA + Integer.parseInt(columnsV[3]);
+                            }
+                            else if (columnsM[2].equals("B")) {
+                                incomeB = incomeB + Integer.parseInt(columnsV[3]);
+                            }
+                            else {
+                                incomeC = incomeC + Integer.parseInt(columnsV[3]);
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(incomeA + " " + incomeB + " " + incomeC);
     }
 }
